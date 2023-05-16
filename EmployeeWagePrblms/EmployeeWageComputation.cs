0a0d564c28aa85adf0a912b4633b1c5e877618eb
naveendowd <119478972+naveendowd@users.noreply.github.com>
2023-05-16 16:23:58 +0530

    EmployeeWage_UC3

--- EmployeeWagePrblms/EmployeeWageComputation.cs
+++ EmployeeWagePrblms/EmployeeWageComputation.cs
@@ -6,23 +6,29 @@ using System.Threading.Tasks;
 
 namespace EmployeeWageComputationProblem
 {
-    class EmployeeWageComputation
+    internal class EmployeeWageComputation
     {
-        public static void EmployeeWage()
+        public static void PartTimeEmployee()
         {
             const int IS_FULL_TIME = 1;
+            const int IS_PART_TIME = 2;
             const int EMP_RATE_PER_HOUR = 20;
             int empHrs = 0;
             int empWage = 0;
 
             Random random = new Random();
 
-            int empCheck = random.Next(0, 2);
+            int empCheck = random.Next(0, 3);
 
             if (empCheck == IS_FULL_TIME)
             {
                 empHrs = 8;
                 Console.WriteLine("Employee is present");
+            }
+            else if (empCheck == IS_PART_TIME)
+            {
+                empHrs = 4;
+                Console.WriteLine("Employee is working as part time");
             }
             else
             {
