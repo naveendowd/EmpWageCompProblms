0466801e91d19ebf193b336dd89135a922f4453e
naveendowd <119478972+naveendowd@users.noreply.github.com>
2023-05-16 16:08:47 +0530

    Employee Wage Program

---
+++ EmployeeWagePrblms/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace EmployeeWageComputationProblem
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to employee wage computation problem");
+        }
+    }
+}
+
